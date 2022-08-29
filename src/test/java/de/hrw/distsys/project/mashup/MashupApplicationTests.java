ce3f33a832b70c7949b38cd9b480213cb824cc20
Yannik <yannik.wegner@stud.hs-ruhrwest.de>
2022-08-29 16:05:15 +0200

    Initial commit

---
+++ src/test/java/de/hrw/distsys/project/mashup/MashupApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hrw.distsys.project.mashup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
